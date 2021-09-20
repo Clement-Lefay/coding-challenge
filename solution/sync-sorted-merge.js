0c0f9b1f99c6e52e6adc25ff3f843fddf4a0976e
Clement LE FAY <clement.le.fay@gmail.com>
2021-09-20 18:57:41 +0200

    Add observation for improvement

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,15 +1,27 @@
-'use strict';
+"use strict";
 
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  /**
+   * Observation on performance
+   * The sort function can handled 1 000 000 sources and
+   * the print function would be the slowest part (taking around 90% to 99.99% of execution time)
+   *
+   * The built-in Sort() looks like cannot process 10 000 000 or sources due to lack of allocated memory
+   * The sort() has to be improved to handled heavy loads
+   */
+  // console.time("sort");
   const sortedLogSource = logSources.sort((a, b) => a.last.date - b.last.date);
+  // console.timeEnd("sort");
 
+  // console.time("print");
   for (let i = 0; i < sortedLogSource.length; i++) {
     const log = sortedLogSource[i];
     printer.print(log.last);
   }
+  // console.timeEnd("print");
 
   printer.done();
-  return console.log('Sync sort complete.');
+  return console.log("Sync sort complete.");
 };
