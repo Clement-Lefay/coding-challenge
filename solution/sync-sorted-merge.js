0b67ded01b8f4ef1e13a70b3f1945a76769d9d9e
Clement LE FAY <clement.le.fay@gmail.com>
2021-09-20 18:18:37 +0200

    First working solution for the syncSortedMerge + Set sources to 100000 to test

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,15 @@
-"use strict";
+'use strict';
 
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  const sortedLogSource = logSources.sort((a, b) => a.last.date - b.last.date);
+
+  for (let i = 0; i < sortedLogSource.length; i++) {
+    const log = sortedLogSource[i];
+    printer.print(log.last);
+  }
+
+  printer.done();
+  return console.log('Sync sort complete.');
 };
