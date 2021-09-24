8bb21ae65bb69e59069a53b7eadb85a193e9d898
Clement LE FAY <clement.le.fay@gmail.com>
2021-09-24 13:39:43 -0400

    Apply the same FS logic to the async version

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const P = require("bluebird");
+const fs = require("fs").promises;
 
 // Heap Sort Algo - originaly fom (https://www.educba.com/sorting-algorithms-in-javascript/)
 let arrLength;
@@ -8,12 +9,10 @@ function heapRoot(items, i) {
   const left = 2 * i + 1;
   const right = 2 * i + 2;
   let max = i;
-  // adapte the condition to match the date as comparator
-  if (left < arrLength && items[left].date > items[max].date) {
+  if (left < arrLength && items[left] > items[max]) {
     max = left;
   }
-  // adapte the condition to match the date as comparator
-  if (right < arrLength && items[right].date > items[max].date) {
+  if (right < arrLength && items[right] > items[max]) {
     max = right;
   }
   if (max != i) {
@@ -29,7 +28,7 @@ function swap(items, index_A, index_B) {
 }
 
 function heapSortAlgo(items) {
-  // console.log("- heap sort start");
+  console.time("heap_sort_async");
   arrLength = items.length;
   for (let i = Math.floor(arrLength / 2); i >= 0; i -= 1) {
     heapRoot(items, i);
@@ -40,23 +39,20 @@ function heapSortAlgo(items) {
     arrLength--;
     heapRoot(items, 0);
   }
-  // console.log("- Heap sort done!");
+  console.timeEnd("heap_sort_async");
 }
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
   return new Promise(async (resolve, reject) => {
-    let logList = [];
+    const tmpPath = "./tmp";
 
     const drainSourceLog = async (logSource) => {
       const log = await logSource.popAsync();
       if (log) {
-        // directly update the logList
-        logList.push({
-          date: log.date.getTime(),
-          msg: log.msg,
-        });
+        // Create a file with these informations
+        await fs.writeFile(`${tmpPath}/${log.date.getTime()}.txt`, log.msg);
         // look for the next log
         return await drainSourceLog(logSource);
       } else {
@@ -65,15 +61,17 @@ module.exports = (logSources, printer) => {
     };
 
     /**
-     * With popAsync, we can maybe have multiple sources calling popAsync concurently
+     * With popAsync, we can have multiple sources calling popAsync concurently
      *
      * so the idea is the same as before
-     *  extract all logs, concurrently is possible
+     *  create a tmp folder
+     *
+     *  extract all logs, concurrently is possible, and create 1 file for each
      *    like for K amount, where K is te number of logSources
      *
-     *  push the log into an array (no split between date and msg, we keep the object to try)
+     *  get the list of files
      *
-     *  sort the array with Heap Sort
+     *  sort this list with Heap Sort
      *
      *  print them all!
      */
@@ -82,6 +80,16 @@ module.exports = (logSources, printer) => {
       resolve(console.log("Please provide a real list of source, this one is empty!"));
     }
 
+    // create new directory
+    try {
+      await fs.mkdir(tmpPath);
+      // first check if directory already exists
+      console.log("Directory is created.");
+    } catch (err) {
+      console.log("Directory already exists.");
+    }
+
+    console.time("drain_log_async");
     // get all logs
     try {
       // Here we are loading each logSource into a promise that is stored in the concurrentPromises
@@ -96,34 +104,51 @@ module.exports = (logSources, printer) => {
       console.error("Something went wrong :/ ", error);
       reject(error);
     }
+    console.timeEnd("drain_log_async");
+
+    // read all files in the tmp folder
+    const files = await fs.readdir(tmpPath);
 
     // sort them chronologicaly
-    heapSortAlgo(logList);
+    heapSortAlgo(files);
 
     // print
-    for (let logIndex = 0; logIndex < logList.length; logIndex++) {
-      printer.print({
-        date: new Date(logList[logIndex].date),
-        msg: logList[logIndex].msg,
-      });
+    for (let logIndex = 0; logIndex < files.length; logIndex++) {
+      try {
+        const data = await fs.readFile(`${tmpPath}/${files[logIndex]}`, "utf-8");
+        // Need to cast the fileName from string to a number that the Date can convert
+        printer.print({
+          date: new Date(+files[logIndex].split(".")[0]),
+          msg: data,
+        });
+      } catch (error) {
+        console.error(error);
+      }
     }
 
+    // remove the directory
+    await fs.rmdir(tmpPath, { recursive: true });
+    console.log(`${tmpPath} is deleted!`);
+
     printer.done();
+    resolve(console.log("Async sort complete."));
+
     /**
      * Remarks
-     * the current implementation cannot pass more than "13 000" logSources due to lack of memory
-     * here are the result of 1 run
-        drain_log_async: 44530.759ms
-        heap_sort_async: 13551.002ms
-        print_async: 2288.748ms
-
-        ***********************************
-        Logs printed:            3115871
-        Time taken (s):          2.288
-        Logs/s:                  1361831.730769231
-        ***********************************
+     * Before applying the FS to this implementation, we were limited to 13 000 logSources.
+     * With how slow it becomes, I have no idea how long it will take
+     * 
+     *  here are the previous result with 13000 (No FS)
+        -        drain_log_async: 44530.759ms
+        -        heap_sort_async: 13551.002ms
+        -        print_async: 2288.748ms
+        -
+        -        ***********************************
+        -        Logs printed:            3115871
+        -        Time taken (s):          2.288
+        -        Logs/s:                  1361831.730769231
+        -        ***********************************
 
      */
-    resolve(console.log("Async sort complete."));
   });
 };
